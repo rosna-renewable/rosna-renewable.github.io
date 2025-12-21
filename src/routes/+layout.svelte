a80e1245b1cdd8e9a3d8e25f72afd8c4a84f8273
Tasin Farhan <tasinfarhan1016@gmail.com>
2025-12-21 18:43:54 +0600

    Added deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		{@render children()}
 	</main>
--- src/routes/Header.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
-</script>
